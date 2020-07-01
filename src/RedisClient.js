405592841682beac98ab1e823ab8f305895874a0
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-07-01 09:20:48 +0530

    made fileds to private which needs inside class only

--- src/RedisClient.js
+++ src/RedisClient.js
@@ -1,29 +1,32 @@
 const { parse } = require('./parseResponse');
 
 class RedisClient {
+  #socket;
+  #callbacks;
+
   constructor(socket, options) {
-    this.socket = socket;
-    this.socket.on('readable', this.gotResponse.bind(this));
-    this.callbacks = [];
+    this.#socket = socket;
+    this.#callbacks = [];
+    this.#socket.on('readable', this.#gotResponse.bind(this));
     options && this.select(options.db);
   }
 
-  gotResponse() {
+  #gotResponse = () => {
     let data = '',
       chunk;
 
-    while ((chunk = this.socket.read())) {
+    while ((chunk = this.#socket.read())) {
       data += chunk;
     }
 
     const responses = parse(data);
-    this.callCallbacks(responses);
-  }
+    this.#callCallbacks(responses);
+  };
 
-  callCallbacks(responses) {
+  #callCallbacks = (responses) => {
     responses.forEach((response) => {
       const { err, res } = response;
-      const callback = this.callbacks.shift();
+      const callback = this.#callbacks.shift();
 
       if (!callback && err) {
         throw err;
@@ -31,42 +34,42 @@ class RedisClient {
 
       callback && callback(err ? err : null, res ? res : null);
     });
-  }
+  };
 
-  sendRequest(command, callback) {
-    this.callbacks.push(callback);
-    this.socket.write(command);
-  }
+  #sendRequest = (command, callback) => {
+    this.#callbacks.push(callback);
+    this.#socket.write(command);
+  };
 
   select(db, callback) {
     const command = `SELECT "${db}"\r\n`;
-    this.sendRequest(command, callback);
+    this.#sendRequest(command, callback);
   }
 
   ping(callback) {
     const command = `PING\r\n`;
-    this.sendRequest(command, callback);
+    this.#sendRequest(command, callback);
   }
 
   set(key, value, callback) {
     const command = `SET ${key} "${value}"\r\n`;
-    this.sendRequest(command, callback);
+    this.#sendRequest(command, callback);
   }
 
   get(key, callback) {
     const command = `GET ${key}\r\n`;
-    this.sendRequest(command, callback);
+    this.#sendRequest(command, callback);
   }
 
   hgetall(key, callback) {
     const command = `HGETALL ${key}\r\n`;
-    this.sendRequest(command, callback);
+    this.#sendRequest(command, callback);
   }
 
   close(callback) {
     const closeInterval = setInterval(() => {
-      if (!this.callbacks.length) {
-        this.socket.end(callback);
+      if (!this.#callbacks.length) {
+        this.#socket.end(callback);
         clearInterval(closeInterval);
       }
     }, 1000);
