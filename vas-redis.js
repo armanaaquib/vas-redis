b7866fe260254fce6c5b15832cab8b98456c9366
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-07-01 19:22:07 +0530

    Added: host and path in options

--- vas-redis.js
+++ vas-redis.js
@@ -1,10 +1,16 @@
 const net = require('net');
 const redisClient = require('./src/RedisClient');
 
+const parseHostPost = (options) => {
+  const host = (options && options.host) || '127.0.0.1';
+  const port = (options && options.port) || 6379;
+
+  return { host, port };
+};
+
 const createClient = function (options) {
-  const socket = net.connect({ host: '127.0.0.1', port: 6379 });
-  const client = new redisClient(socket, options);
-  return client;
+  const socket = net.connect(parseHostPost(options));
+  return new redisClient(socket, options);
 };
 
 module.exports = { createClient };
